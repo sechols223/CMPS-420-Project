f1599a7cef2c5dd0ca05c262fe9259064d321fd1
Peyton Perry <peyton.perry@selu.edu>
2024-10-24 11:47:38 -0500

    Started home page
    features added:
    added header with a small amount of navigation (only home button, does not lead anywhere :) )
    added log in and sign up button
    added Sign up button opens a modal with text input for future form
    -currently only has email listed for text input

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,113 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import {
+  IconBook,
+  IconChartPie3,
+  IconChevronDown,
+  IconCode,
+  IconCoin,
+  IconFingerprint,
+  IconNotification,
+} from '@tabler/icons-react';
+import {
+  Anchor,
+  Box,
+  Burger,
+  Button,
+  Center,
+  Collapse,
+  Divider,
+  Drawer,
+  Group,
+  HoverCard,
+  Modal,
+  rem,
+  ScrollArea,
+  SimpleGrid,
+  Text,
+  TextInput,
+  ThemeIcon,
+  UnstyledButton,
+  useMantineTheme,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from '../CSS/HeaderMegaMenu.module.css';
+
+const mockdata = [
+  {
+    icon: IconCode,
+    title: 'Open source',
+    description: 'This Pokémon’s cry is very loud and distracting',
+  },
+  {
+    icon: IconCoin,
+    title: 'Free for everyone',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+  },
+  {
+    icon: IconBook,
+    title: 'Documentation',
+    description: 'Yanma is capable of seeing 360 degrees without',
+  },
+  {
+    icon: IconFingerprint,
+    title: 'Security',
+    description: 'The shell’s rounded shape and the grooves on its.',
+  },
+  {
+    icon: IconChartPie3,
+    title: 'Analytics',
+    description: 'This Pokémon uses its flying ability to quickly chase',
+  },
+  {
+    icon: IconNotification,
+    title: 'Notifications',
+    description: 'Combusken battles with the intensely hot flames it spews',
+  },
+];
 
 export function HomePage() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const theme = useMantineTheme();
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const links = mockdata.map((item) => (
+    <UnstyledButton className={classes.subLink} key={item.title}>
+      <Group wrap="nowrap" align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
+          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
+        </ThemeIcon>
+        <div>
+          <Text size="sm" fw={500}>
+            {item.title}
+          </Text>
+          <Text size="xs" c="dimmed">
+            {item.description}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+  ));
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <Box pb={120}>
+      <header className={classes.header}>
+        <Group justify="space-between" h="100%">
+          <Group h="100%" gap={0} visibleFrom="sm">
+            <a href="#" className={classes.link}>
+              Home
+            </a>
+          </Group>
+
+          <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Modal opened={opened} onClose={close} title="Sign up">
+              {<TextInput withAsterisk label="Email" placeholder="ex. user@gmail.com" />}
+            </Modal>
+            <Button onClick={open}>Sign up</Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+        </Group>
+      </header>
+    </Box>
   );
 }
