e452c92f71963df4701263406c37cfad6f0e2328
Peyton Perry <peyton.perry@selu.edu>
2024-10-28 15:40:43 -0500

    Changes to Home page, added General welcome page
    Removed the login and sign up stuff from user home page. this will be the 'logged in' page.
    Added a new welcome page which will be the login or sign up page.
     -This is unfinished - need to fix the login and sign up button positions.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { GetStartedPage } from './pages/GetStarted.page';
 import { HomePage } from './pages/Home.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <GetStartedPage />,
+  },
+  {
+    path: '/home',
     element: <HomePage />,
   },
 ]);
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -8,22 +8,13 @@ import {
   IconNotification,
 } from '@tabler/icons-react';
 import {
-  Anchor,
   Box,
   Burger,
   Button,
-  Center,
-  Collapse,
-  Divider,
-  Drawer,
   Group,
-  HoverCard,
   Modal,
   rem,
-  ScrollArea,
-  SimpleGrid,
   Text,
-  TextInput,
   ThemeIcon,
   UnstyledButton,
   useMantineTheme,
@@ -32,82 +23,24 @@ import { useDisclosure } from '@mantine/hooks';
 import { RegistrationForm } from './RegistrationForm';
 import classes from '../CSS/HeaderMegaMenu.module.css';
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
-
 export function HomePage() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
   const theme = useMantineTheme();
   const [opened, { open, close }] = useDisclosure(false);
 
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
-
   return (
     <Box pb={120}>
       <header className={classes.header}>
-        <Group justify="space-between" h="100%">
-          <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="#" className={classes.link}>
-              Home
-            </a>
-          </Group>
-
-          <Group visibleFrom="sm">
-            <Button variant="default">Log in</Button>
-            <Modal opened={opened} onClose={close} title="Sign up">
-              <RegistrationForm />
-              {/* Use the RegistrationForm component */}
-            </Modal>
-            <Button onClick={open}>Sign up</Button>
-          </Group>
-
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+        <Group h="100%" gap={0} visibleFrom="sm">
+          <a href="#" className={classes.link}>
+            Home
+          </a>
+          <a href="#" className={classes.link}>
+            Gallery
+          </a>
+          <a href="#" className={classes.link}>
+            Albums
+          </a>
         </Group>
       </header>
     </Box>
