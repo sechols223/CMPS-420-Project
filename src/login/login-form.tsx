6cc6bdc8577a8631ed8e877fa84b48926bbb72e0
Peyton Perry <peyton.perry@selu.edu>
2024-10-28 23:41:12 -0500

    Im just trying stuff, basically none of this is usable
    Dont use this garbage, I'll work out a solution at some point.
    It does work though, its just insanely ugly.
    Sam why am I using hook form????

--- src/login/login-form.tsx
+++ src/login/login-form.tsx
@@ -1,33 +1,23 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Button, TextInput } from '@mantine/core';
-import styles from './RegistrationForm.module.css';
-
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-};
+import styles from '../CSS/forms.css';
 
 export function LoginForm() {
   const {
     register,
-    handleSubmit,
-    watch,
     formState: { errors },
+    getValues,
+    handleSubmit,
   } = useForm();
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
-  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register('example')} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register('exampleRequired', { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type="submit" />
-    </form>
+    <div className="loginform">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input {...register('password', { required: 'Password is required' })} />
+        {errors.password && <p style={{ color: 'white' }}></p>}
+        <button type="submit">Trigger</button>
+      </form>
+    </div>
   );
 }
