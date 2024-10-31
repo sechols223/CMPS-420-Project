500b266973f60031a2354239f877770e8c2020cb
Peyton Perry <peyton.perry@selu.edu>
2024-10-31 11:55:48 -0500

    Drag and drop added to Albums page.
    Added the drag and drop - will be making it into its own card soon.

--- client/src/pages/album-page/album-page.tsx
+++ client/src/pages/album-page/album-page.tsx
@@ -1,6 +1,20 @@
+import { useRef } from 'react';
+import { IconCloudUpload, IconDownload, IconX } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Center, Group, SimpleGrid, Stack, Title } from '@mantine/core';
-import classes from '../GalleryPage/Gallery-Page.module.css';
+import {
+  Box,
+  Button,
+  Center,
+  Group,
+  rem,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from '@mantine/core';
+import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
+import classes from './DropzoneButton.module.css';
 
 export default function AlbumPage() {
   let navigate = useNavigate();
@@ -9,13 +23,15 @@ export default function AlbumPage() {
     navigate(path);
   };
   const routeToAlbums = () => {
-    let path = '/albums';
+    let path = '/albums ';
     navigate(path);
   };
   const routeToHome = () => {
     let path = '/home';
     navigate(path);
   };
+  const theme = useMantineTheme();
+  const openRef = useRef<() => void>(null);
   return (
     <>
       <Box pb={120}>
@@ -34,6 +50,46 @@ export default function AlbumPage() {
             <Center>
               <Title> Albums </Title>
             </Center>
+            <Group>
+              <Dropzone
+                openRef={openRef}
+                onDrop={() => {}}
+                className={classes.dropzone}
+                radius="md"
+                accept={[MIME_TYPES.pdf]}
+                maxSize={30 * 1024 ** 2}
+              >
+                <Group>
+                  <Stack>
+                    <Group justify="center">
+                      <Dropzone.Accept>
+                        <IconDownload
+                          style={{ width: rem(50), height: rem(50) }}
+                          color={theme.colors.blue[6]}
+                          stroke={1.5}
+                        />
+                      </Dropzone.Accept>
+                      <Dropzone.Reject>
+                        <IconX
+                          style={{ width: rem(50), height: rem(50) }}
+                          color={theme.colors.red[6]}
+                          stroke={1.5}
+                        />
+                      </Dropzone.Reject>
+                      <Dropzone.Idle>
+                        <IconCloudUpload style={{ width: rem(50), height: rem(50) }} stroke={1.5} />
+                      </Dropzone.Idle>
+                    </Group>
+
+                    <Text ta="center" fw={700} fz="lg" mt="xl">
+                      <Dropzone.Accept>Drop files here</Dropzone.Accept>
+                      <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+                      <Dropzone.Idle>Upload files</Dropzone.Idle>
+                    </Text>
+                  </Stack>
+                </Group>
+              </Dropzone>
+            </Group>
             <SimpleGrid cols={{ base: 1, sm: 2 }}>{}</SimpleGrid>
           </Stack>
         </Center>
