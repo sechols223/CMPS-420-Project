83870f63998782ada4fc14680b6620d3aa8f0b76
Peyton Perry <peyton.perry@selu.edu>
2024-10-31 11:08:03 -0500

    Nav bar changes and gallery page styling
    reapplied the changes made to gallery page and changed the nav bar to pre samuel changes. Should be pretty much ready for backend now. Waiting on sam.

--- client/src/pages/home-page/Home.page.tsx
+++ client/src/pages/home-page/Home.page.tsx
@@ -1,29 +1,28 @@
-import { Box, Group } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Group } from '@mantine/core';
 import classes from '../../CSS/HeaderMegaMenu.module.css';
 
 export function HomePage() {
+  let navigate = useNavigate();
+  const routeToGallery = () => {
+    let path = '/gallery';
+    navigate(path);
+  };
+  const routeToAlbums = () => {
+    let path = '/';
+    navigate(path);
+  };
+  const routeToHome = () => {
+    let path = '/home';
+    navigate(path);
+  };
   return (
     <Box pb={120}>
       <header className={classes.header}>
-        <Group h="100%" gap={0} visibleFrom="sm">
-          <a
-            className={classes.link}
-            href="#"
-          >
-            Home
-          </a>
-          <a 
-            className={classes.link} 
-            href="/gallery"
-          >
-            Gallery
-          </a>
-          <a 
-            className={classes.link}
-            href="/albums"
-          >
-            Albums
-          </a>
+        <Group h="100%" gap={15} visibleFrom="sm">
+          <Button onClick={routeToHome}>Home</Button>
+          <Button onClick={routeToGallery}>Gallery</Button>
+          <Button>Albums</Button>
         </Group>
       </header>
     </Box>
