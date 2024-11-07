81a7051394eff72a3948fac3e832cf03ed41676c
Jai Brown <jai.brown@selu.edu>
2024-11-07 13:52:09 -0600

    trying navbar & route stuff. Do not pull, its super janky and broken, will fix later. Also added carousel to homepage :)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
+
 
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { Router2 } from './Router2';
+
+
+const isLoggedIn = true;
+
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      {isLoggedIn === false ?
+        (
+          <Router2/> 
+        ) :
+        (
+          <Router />
+        )
+      }
+      
     </MantineProvider>
   );
 }
--- client/src/pages/GalleryPage/Gallery-Page.tsx
+++ client/src/pages/GalleryPage/Gallery-Page.tsx
@@ -50,22 +50,7 @@ const mockdata = [
 
 export default function GalleryPage() {
   let navigate = useNavigate();
-  const routeToGallery = () => {
-    let path = '/gallery';
-    navigate(path);
-  };
-  const routeToAlbums = () => {
-    let path = '/albums';
-    navigate(path);
-  };
-  const routeToHome = () => {
-    let path = '/home';
-    navigate(path);
-  };
-  const routeToUserHome = () => {
-    let path = '/home';
-    navigate(path);
-  };
+
   const cards = mockdata.map((article) => (
     <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
       <AspectRatio ratio={1920 / 1080}>
@@ -83,51 +68,7 @@ export default function GalleryPage() {
   return (
     <>
       {' '}
-      <Box pb={50}>
-        <header className={classes.header}>
-          <div className={classes.logoContainer}>
-            <img src={logo} alt="Logo" className={classes.logo} />
-          </div>
-          <Group h="100%" gap={10} visibleFrom="sm">
-            <Button
-              title="Home"
-              onClick={routeToUserHome}
-              style={{ backgroundColor: '#ff914d', color: '#39445a', fontWeight: 'bold' }}
-            >
-              Home
-            </Button>
-            <Button
-              title="Gallery"
-              onClick={routeToGallery}
-              style={{ backgroundColor: '#ff914d', color: '#39445a', fontWeight: 'bold' }}
-            >
-              Gallery
-            </Button>
-            <Button
-              title="Albums"
-              onClick={routeToAlbums}
-              style={{ backgroundColor: '#ff914d', color: '#39445a', fontWeight: 'bold' }}
-            >
-              Albums
-            </Button>
-          </Group>
-          <div className={classes.searchBar}>
-            <TextInput
-              radius="xl"
-              size="md"
-              placeholder="Search images, albums, and more"
-              rightSectionWidth={42}
-              leftSection={<IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />}
-              rightSection={
-                <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
-                  <IconArrowRight style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                </ActionIcon>
-              }
-              style={{ width: '40%' }}
-            />
-          </div>
-        </header>
-      </Box>
+
 
       <Group>
         <Center>
