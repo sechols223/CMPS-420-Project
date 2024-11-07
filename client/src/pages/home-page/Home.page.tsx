e90af79a39b5987271ea0057c6419dc132421df6
Jai Brown <jai.brown@selu.edu>
2024-11-07 09:43:47 -0600

    carousel in home page

--- client/src/pages/home-page/Home.page.tsx
+++ client/src/pages/home-page/Home.page.tsx
@@ -1,9 +1,85 @@
 import { useNavigate } from 'react-router-dom';
-import { ActionIcon, Box, Button, Group, rem, TextInput } from '@mantine/core';
-import classes from '../../CSS/HeaderMegaMenu.module.css';
+import { ActionIcon, Box, Button, Group, Paper, rem, TextInput, useMantineTheme, Text, Title } from '@mantine/core';
+import '../../CSS/HeaderMegaMenu.module.css';
 import logo from '../../components/Images/Logo_Small@2x.png'; // Adjust the path as needed
 import { IconArrowRight, IconSearch } from '@tabler/icons-react';
 import { theme } from '@/theme';
+import classes from '../home-page/Home.page.module.css'
+import { useMediaQuery } from '@mantine/hooks';
+import { Carousel } from '@mantine/carousel';
+import '@mantine/carousel/styles.css';
+
+
+
+
+interface CardProps {
+  image: string;
+  title: string;
+  category: string;
+}
+
+function Card({ image, title, category }: CardProps) {
+  return (
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      style={{ backgroundImage: `url(${image})` }}
+      className={classes.card}
+    >
+      <div>
+        <Text className={classes.category} size="xs">
+          {category}
+        </Text>
+        <Title order={3} className={classes.title}>
+          {title}
+        </Title>
+      </div>
+    </Paper>
+  );
+}
+
+const data = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best forests to visit in North America',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Hawaii beaches review: better than you think',
+    category: 'beach',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Mountains at night: 12 best locations to enjoy the view',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Aurora in Norway: when to visit for best experience',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best places to visit this winter',
+    category: 'tourism',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Active volcanos reviews: travel at your own risk',
+    category: 'nature',
+  },
+];
+
+
+
 export function HomePage() {
   let navigate = useNavigate();
   const routeToGallery = () => {
@@ -14,14 +90,20 @@ export function HomePage() {
     let path = '/albums';
     navigate(path);
   };
-  const routeToHome = () => {
-    let path = '/home';
-    navigate(path);
-  };
   const routeToUserHome = () => {
     let path = '/home';
     navigate(path);
   };
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const slides = data.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <Card {...item} />
+    </Carousel.Slide>
+  ));
+
+
+
   return (
     <>
     {' '}
@@ -70,5 +152,18 @@ export function HomePage() {
           </div>
         </header>
       </Box>
-      </>
+
+      <Box pb={50}>
+
+      <Carousel
+      slideSize={{ base: '80%', sm: '50%' }}
+      slideGap={{ base: rem(2), sm: 'xl' }}
+      align="start"
+      slidesToScroll={mobile ? 1 : 2}
+      loop
+    >
+      {slides}
+    </Carousel>
+      </Box>
+  </>
 )}
