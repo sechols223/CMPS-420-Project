6a47100cd35ddf0739b57e307880d92764abe340
VannaMoore <vannamoore@gmail.com>
2024-11-19 11:59:04 -0600

    Updated dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,16 @@
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 
-
 import { MantineProvider } from '@mantine/core';
 import { Router, Router2 } from './Router';
 import { theme } from './theme';
 
-
-
-const isLoggedIn = true;
-
+const isLoggedIn = false;
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      {isLoggedIn === false ?
-        (
-          <Router2/> 
-        ) :
-        (
-          <Router />
-        )
-      }
-      
+      {isLoggedIn === false ? <Router2 /> : <Router />}
     </MantineProvider>
   );
 }
--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,19 +1,18 @@
+import { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom';
+import { NavBar } from './components/NavBar/Nav-Bar';
 import { LoginForm } from './login/login-form';
+import { AlbumPage } from './pages/AlbumPage/AlbumPage';
 import GalleryPage from './pages/GalleryPage/Gallery-Page';
 import { GetStartedPage } from './pages/get-started-page/GetStarted.page';
 import { HomePage } from './pages/home-page/Home.page';
 import { RegistrationForm } from './registration/RegistrationForm';
-import { AlbumPage } from './pages/AlbumPage/AlbumPage';
-import { NavBar } from './components/NavBar/Nav-Bar';
-import { useEffect } from 'react';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavBar/>,
-    children: 
-    [
+    element: <NavBar />,
+    children: [
       {
         path: '/',
         element: <HomePage />,
@@ -32,29 +31,47 @@ const router = createBrowserRouter([
       },
       {
         path: '/albums',
-        element: <AlbumPage/>
+        element: <AlbumPage />,
       },
-    ]
+    ],
   },
   {
     path: '/get-started',
-    element: <GetStartedPage/>
-  }
+    element: <GetStartedPage />,
+  },
 ]);
 
 const router2 = createBrowserRouter([
   {
     path: '/',
-    element: <GetStartedPage/>
+    element: <GetStartedPage />,
   },
-  
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/gallery',
+    element: <GalleryPage />,
+  },
+  {
+    path: '/loginform',
+    element: <LoginForm />,
+  },
+  {
+    path: '/signupform',
+    element: <RegistrationForm />,
+  },
+  {
+    path: '/albums',
+    element: <AlbumPage />,
+  },
 ]);
 
 export function Router() {
-
   return <RouterProvider router={router} />;
 }
 
 export function Router2() {
-  return <RouterProvider router={router2} />
+  return <RouterProvider router={router2} />;
 }
