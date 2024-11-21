e7cb4a1f9856a1f03b0dbe41b3fd183359375e14
Samuel Echols <samuel.echols@selu.edu>
2024-11-21 09:51:24 -0600

    feat: added types

---
+++ client/src/types.ts
@@ -0,0 +1,20 @@
+﻿
+export type PaginatedResponse<T> = {
+	count: number;
+	items: T[]
+}
+
+export type ImageGetDto = {
+	id: string;
+	name: string;
+	tags: string[];
+	description: string;
+	imageData: string;
+	location?: string;
+}
+
+export type Album = {
+	id: string;
+	name: string;
+	images: ImageGetDto[]
+}
