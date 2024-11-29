173a600654fbd61f1a9e5342c89e61e95b11e36f
Samuel Echols <samuel.echols@selu.edu>
2024-11-29 12:05:41 -0600

    feat: added prettier config

---
+++ client/.prettierrc.js
@@ -0,0 +1,15 @@
+﻿
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  parser: 'typescript',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 80,
+  jsxSingleQuote: false,
+}
+
+module.exports = config;
