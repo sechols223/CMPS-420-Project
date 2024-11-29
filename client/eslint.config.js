4f3576e29932964894e433d50094478a57b10323
Samuel Echols <samuel.echols@selu.edu>
2024-11-29 12:05:33 -0600

    feat: modified eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,7 +1,18 @@
-import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 export default tseslint.config(
-  ...mantine,
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+    }
+  },
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
 );
