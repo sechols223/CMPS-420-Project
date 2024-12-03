c2220e07ed5fb1a131ec2467487c764b9c8dac58
Samuel Echols <samuel.echols@selu.edu>
2024-12-02 20:01:40 -0600

    Feat/server (#26)
    * server
    
    * server
    
    * requirements
    
    * azure
    
    * feat: fixed imports
    
    * feat: fixed imports
    
    * deleted ds folder
    
    * feat: added types
    
    * feat: added dependencies
    
    * feat: integrations
    
    * feat: added conda env file
    
    * feat: added .lock files to gitignore
    
    * feat: modified eslint config
    
    * feat: added prettier config
    
    * feat: added packages
    
    * feat: misc modifications
    
    * Corrected the type on Album
    
    * Corrected the type on Album

--- server/server/routers/albums_router.py
+++ server/server/routers/albums_router.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from bson import ObjectId
 from fastapi import APIRouter
@@ -16,14 +16,14 @@ album_db = database['Albums']
 
 class Album(BaseDBSchema):
     name: str
-    imageIds: List[PyObjectId]
-    images: List[ImageGetDto]
+    imageIds: Optional[List[PyObjectId]] = None
+    images: Optional[List[ImageGetDto]] = None
 
 class AlbumCreateDto(BaseModel):
     name: str
 
 class AlbumImageListDto(BaseModel):
-    image_ids: list[str]
+    image_ids: Optional[list[str]] = None
 
 def _get_albums() -> List[Album]:
     """
@@ -151,6 +151,7 @@ async def create_album(dto: AlbumCreateDto):
 
 @router.post("/api/albums/{id}")
 async def add_image_to_album(id: str, dto: AlbumImageListDto):
+
     if not ObjectId.is_valid(id):
         return JSONResponse(content={"message": "Album not found"}, status_code=404)
 
@@ -160,8 +161,77 @@ async def add_image_to_album(id: str, dto: AlbumImageListDto):
 
     ids_to_insert = [id for id in dto.image_ids if id not in album.imageIds]
 
-    # Convert string ID to ObjectId 
-    album_db.update_one(  
+    database['albums'].update_one(
+        {"_id": id},
+        { "$push": { "imageIds": {'$each': ids_to_insert} } }
+    )
+
+
+    return album
+
+@router.delete('/api/albums/{id}/remove-single/{image_id}')
+async def remove_image_from_album(id:str, image_id: str):
+    if not ObjectId.is_valid(id) or not ObjectId.is_valid(image_id):
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    album = _get_album_by_id(id)
+    if album is None:
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    update_result = album_db.update_one({'_id': ObjectId(id)}, {
+        '$pull':
+            {
+                'imageIds': { '$in': [image_id] }
+            }
+    })
+
+    album = _get_album_by_id(id)
+    return album
+
+@router.put('/api/albums/{id}/remove')
+async def remove_images_from_album(id: str, dto: AlbumImageListDto) -> Album:
+
+    if not ObjectId.is_valid(id):
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    album = _get_album_by_id(id)
+    if album is None:
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    update_result = album_db.update_one({'_id': ObjectId(id)}, {
+        '$pull':
+        {
+            'imageIds': { '$in': dto.image_ids }
+        }
+    })
+
+    album = _get_album_by_id(id)
+    return album
+
+@router.post('/api/albums')
+async def create_album(dto: AlbumCreateDto):
+    found_album = album_db.find_one({'name': f'/^{dto.name}$/i'})
+    if found_album is not None:
+        return JSONResponse({'message': 'An album already exists with this name'}, 400)
+
+    inserted_album = album_db.insert_one(dto.__dict__)
+    album = Album(**album_db.find_one(inserted_album.inserted_id))
+
+    return album
+
+@router.post("/api/albums/{id}")
+async def add_image_to_album(id: str, dto: AlbumImageListDto):
+    if not ObjectId.is_valid(id):
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    album = _get_album_by_id(id)
+    if album is None:
+        return JSONResponse(content={"message": "Album not found"}, status_code=404)
+
+    ids_to_insert = [id for id in dto.image_ids if id not in album.imageIds]
+
+    # Convert string ID to ObjectId
+    album_db.update_one(
         {"_id": ObjectId(id)},  # Convert id to ObjectId
         {"$push": {"imageIds": {'$each': ids_to_insert}}}
     )
